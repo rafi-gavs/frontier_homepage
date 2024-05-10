8c68fa4ae37399a41d5153e108b5552e5383a44b
shubham-gslab <shubham.hande@gslab.com>
2024-05-10 20:39:23 +0530

    named routes added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:frontier_homepage/routes.dart';
 import 'package:frontier_homepage/screens/flight_status/flight_status_page.dart';
-import 'package:frontier_homepage/screens/home_page.dart';
-import 'package:google_fonts/google_fonts.dart';
-// import 'package:native_shared_preferences/native_shared_preferences.dart';
-
-// late NativeSharedPreferences nPrefs;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  homePage();
+  runApp(const MyApp());
 }
 
-void homePage() {
-  runApp(const MyApp(child: HomePage()));
-}
 
-void flightStatusPage() {
-  runApp(const MyApp(child: FlightStatusPage()));
-}
 
 class MyApp extends StatelessWidget {
-  final Widget child;
   const MyApp({
     super.key,
-    required this.child,
   });
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Frontier Airlines',
-      home: child,
+      onGenerateRoute: Routes.generateRoute,
+      initialRoute:Routes.home,
     );
   }
 }
