31e101cfa39b69143defa159e388f18e35ed33c8
shubham-gslab <shubham.hande@gslab.com>
2024-05-11 18:01:59 +0530

    initial route updated.

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Frontier Airlines',
       onGenerateRoute: Routes.generateRoute,
-      initialRoute:Routes.flightStatus,
+      initialRoute:Routes.home,
     );
   }
 }
