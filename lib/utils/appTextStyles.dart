c15ee65f919149a8e226b73d0f5f0bd6e5e6906a
praseethaps <praseetha.ps@gavstech.com>
2024-05-13 22:37:17 +0530

    "Common Text Theme"

---
+++ lib/utils/appTextStyles.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:frontier_homepage/utils/appcolor.dart';
+
+class AppTextStyles{
+  static const TextStyle heading1 = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Montserrat',
+    color:  AppColor.stringBlackColor
+  );
+  static const TextStyle heading2 = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle subHeading=TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Poppins',
+    color:  AppColor.stringBlackColor
+  );
+  static const TextStyle bodyText1 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Montserrat'
+  );
+  static const TextStyle bodyText2 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins'
+  );
+  static const TextStyle bodyText3=TextStyle(
+     fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Poppins'
+  );
+  static const TextStyle bodyText4=TextStyle(
+     fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Poppins'
+  );
+  
+  static const TextStyle buttonText=TextStyle(
+    fontSize: 19.0,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Poppins'
+  );
+  static const TextStyle cardHeading1=TextStyle(
+     fontSize: 24.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Montserrat',
+    color:  AppColor.stringBlackColor
+  );
+  static const TextStyle cardHeading2=TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Poppins',
+    color:  AppColor.stringBlackColor
+  );
+  static const TextStyle small=TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Poppins',
+  );
+   static const TextStyle large=TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Poppins',
+  );
+}
